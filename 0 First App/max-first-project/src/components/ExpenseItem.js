353ee7937b12e4928f7f86958901baea6cbb6ad8
aathym <aat8001@gmail.com>
2022-08-06 20:03:29 +0530

    show dynamic data

--- 0 First App/max-first-project/src/components/ExpenseItem.js
+++ 0 First App/max-first-project/src/components/ExpenseItem.js
@@ -1,11 +1,18 @@
 import './ExpenseItem.css';
 function ExpenseItem() {
+
+
+    const expenseDate= new Date(2022,7,28);
+    const expenseTitle = 'Car Insurance'
+    const expenseAmount = 123.8
+
+
   return (
     <div className='expense-item'>
-      <div>Aug 10 2022</div>
+      <div>{expenseDate.toISOString()}</div>
       <div className='expense-item__description'>
-        <h2>Car Insurance</h2>
-        <div className='expense-item__price'>$123.45</div>
+        <h2>{expenseTitle}</h2>
+        <div className='expense-item__price'>$ {expenseAmount}</div>
       </div>
     </div>
   );
