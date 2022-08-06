68fe00e987878398e886247c62de39f43288a761
aathym <aat8001@gmail.com>
2022-08-06 19:09:26 +0530

    add more jsx

--- 0 First App/max-first-project/src/components/ExpenseItem.js
+++ 0 First App/max-first-project/src/components/ExpenseItem.js
@@ -1,12 +1,13 @@
-function ExpenseItem(){
-
-
-
-    return (
-
-        <div> <h2>I am the first component .</h2></div>
-    )
+function ExpenseItem() {
+  return (
+    <div>
+      <div>Date</div>
+      <div>
+        <h2>Car Insurance</h2>
+        <div>$123.45</div>
+      </div>
+    </div>
+  );
 }
 
-
-export default  ExpenseItem
+export default ExpenseItem;
