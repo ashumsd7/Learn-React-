50ceae5a76fadf06ba49deab3ce02a20a3d8d0f6
aathym <aat8001@gmail.com>
2022-08-07 00:20:21 +0530

    improve prop receiving

--- 0 First App/max-first-project/src/components/ExpenseItem.js
+++ 0 First App/max-first-project/src/components/ExpenseItem.js
@@ -1,18 +1,20 @@
-import './ExpenseItem.css';
+import "./ExpenseItem.css";
 function ExpenseItem(props) {
-
-
-    const expenseDate= props.date;
-    const expenseTitle = props.title
-    const expenseAmount = props.amount
-
+  const month = props.date.toLocaleString("en-US", { month: "long" });
+  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+  const year = props.date.getFullYear();
 
   return (
-    <div className='expense-item'>
-      <div>{expenseDate.toISOString()}</div>
-      <div className='expense-item__description'>
-        <h2>{expenseTitle}</h2>
-        <div className='expense-item__price'>$ {expenseAmount}</div>
+    <div className="expense-item">
+      <div>
+        {/* {expenseDate.toISOString()} */}
+        <div>{month}</div>
+        <div>{day}</div>
+        <div>{year}</div>
+      </div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">$ {props.amount}</div>
       </div>
     </div>
   );
