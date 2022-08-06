b1ead7321264c9d4841dc7994d74a4da78247148
aathym <aat8001@gmail.com>
2022-08-07 00:31:19 +0530

    Splitting components

--- 0 First App/max-first-project/src/components/ExpenseItem.js
+++ 0 First App/max-first-project/src/components/ExpenseItem.js
@@ -1,16 +1,12 @@
 import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
 function ExpenseItem(props) {
-  const month = props.date.toLocaleString("en-US", { month: "long" });
-  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
-  const year = props.date.getFullYear();
 
   return (
     <div className="expense-item">
       <div>
-        {/* {expenseDate.toISOString()} */}
-        <div>{month}</div>
-        <div>{day}</div>
-        <div>{year}</div>
+    
+       <ExpenseDate date={props.date}></ExpenseDate>
       </div>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
