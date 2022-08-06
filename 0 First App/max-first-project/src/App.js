92adbc8d618bc0e6ebb729ed73d157f01b99380f
aathym <aat8001@gmail.com>
2022-08-06 18:53:09 +0530

    create component export and import

--- 0 First App/max-first-project/src/App.js
+++ 0 First App/max-first-project/src/App.js
@@ -1,6 +1,9 @@
+import ExpenseItem from './components/ExpenseItem'
+
 function App() {
   return (
     <div>
+      <ExpenseItem/>
       <h2>Let's get started!</h2>
       <h4>I am also visible</h4>
     </div>
---
+++ 0 First App/max-first-project/src/components/ExpenseItem.js
@@ -0,0 +1,12 @@
+function ExpenseItem(){
+
+
+
+    return (
+
+        <div> <h2>I am the first component .</h2></div>
+    )
+}
+
+
+export default  ExpenseItem
