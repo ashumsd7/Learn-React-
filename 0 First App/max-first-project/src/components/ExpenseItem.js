b38152f17e8bec00a04dbac30eaa2934562fa202
aathym <aat8001@gmail.com>
2022-08-06 19:15:15 +0530

    add styles to components using className

--- 0 First App/max-first-project/src/components/ExpenseItem.js
+++ 0 First App/max-first-project/src/components/ExpenseItem.js
@@ -1,10 +1,11 @@
+import './ExpenseItem.css';
 function ExpenseItem() {
   return (
-    <div>
-      <div>Date</div>
-      <div>
+    <div className='expense-item'>
+      <div>Aug 10 2022</div>
+      <div className='expense-item__description'>
         <h2>Car Insurance</h2>
-        <div>$123.45</div>
+        <div className='expense-item__price'>$123.45</div>
       </div>
     </div>
   );
