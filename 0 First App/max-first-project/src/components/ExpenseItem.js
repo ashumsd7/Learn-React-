dc6e46619a1e064a94ba159b0c8a67ef050e3769
aathym <aat8001@gmail.com>
2022-08-07 13:21:54 +0530

    useState Hook

--- 0 First App/max-first-project/src/components/ExpenseItem.js
+++ 0 First App/max-first-project/src/components/ExpenseItem.js
@@ -1,20 +1,21 @@
+import React, { useState } from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 
-const clickHandler= ()=>{
-    console.log("Clicked")
-    alert("Clicked")
-}
 function ExpenseItem(props) {
+  const [title, setTitle] = useState(props.title);
+
+  const clickHandler = () => {
+    setTitle("NEW TITLE");
+  };
 
   return (
     <div className="expense-item">
       <div>
-    
-       <ExpenseDate date={props.date}></ExpenseDate>
+        <ExpenseDate date={props.date}></ExpenseDate>
       </div>
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">$ {props.amount}</div>
       </div>
 
