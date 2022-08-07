126ef0447196a5f91c41e099f6adda9b07009e05
aathym <aat8001@gmail.com>
2022-08-07 16:47:18 +0530

    Add change event to input

--- 0 First App/max-first-project/src/components/NewExpense/ExpenseForm.js
+++ 0 First App/max-first-project/src/components/NewExpense/ExpenseForm.js
@@ -1,12 +1,23 @@
 import React from "react";
 import "./ExpenseForm.css";
 const ExpenseForm = () => {
+
+
+    const titleChangeHandler= (event)=>{
+
+        console.log(event.target.value)
+
+    }
+
+
+
+
   return (
     <form>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" />
+          <input onChange={titleChangeHandler} type="text" />
         </div>
 
         <div className="new-expense__control">
