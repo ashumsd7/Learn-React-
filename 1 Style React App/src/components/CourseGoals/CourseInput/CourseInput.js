6b13ff1adfdc5f172bc708726d2350e53a430335
aathym <aat8001@gmail.com>
2022-08-08 19:08:23 +0530

    init

--- 1 Style React App/src/components/CourseGoals/CourseInput/CourseInput.js
+++ 1 Style React App/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -6,19 +6,25 @@ import './CourseInput.css';
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
 
+ const [isValid,setIsValid]= useState(true)
+
   const goalInputChangeHandler = event => {
     setEnteredValue(event.target.value);
   };
 
   const formSubmitHandler = event => {
     event.preventDefault();
+    if(enteredValue.trim().length===0){
+      setIsValid(false)
+      return;
+    }
     props.onAddGoal(enteredValue);
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
-        <label>Course Goal</label>
+        <label style={{color:!isValid ?'red' : 'black'}}>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
       </div>
       <Button type="submit">Add Goal</Button>
