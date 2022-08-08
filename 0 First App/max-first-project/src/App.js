90888463a14e42025891cd2bb627bbb64d0e2783
aathym <aat8001@gmail.com>
2022-08-08 10:58:52 +0530

    Render List

--- 0 First App/max-first-project/src/App.js
+++ 0 First App/max-first-project/src/App.js
@@ -32,29 +32,18 @@ function App() {
   return (
     <div>
       <NewExpense onAddExpense={addExpensesHandler} />
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      />
 
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      />
 
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      />
+      {(expenses.map(expense => <ExpenseItem title={expense.title}
+         amount={expense.amount}
+         date={expense.date}
+      />))}
 
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      />
+
+
+
+
+
     </div>
   );
 }
