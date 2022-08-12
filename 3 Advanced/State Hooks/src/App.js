f226a787953d2605c4f162239b08e2fcaca954cb
aathym <aat8001@gmail.com>
2022-08-12 10:16:37 +0530

    login setup in useEffect

--- 3 Advanced/State Hooks/src/App.js
+++ 3 Advanced/State Hooks/src/App.js
@@ -1,20 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from "react";
 
-import Login from './components/Login/Login';
-import Home from './components/Home/Home';
-import MainHeader from './components/MainHeader/MainHeader';
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import MainHeader from "./components/MainHeader/MainHeader";
 
 function App() {
+  const ifStoredUserLoggedIn = localStorage.getItem("isLoggedIn");
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+
+  // useEffect runs after every components evaluation
+  useEffect(() => {
+    console.log(">>>>>>.", ifStoredUserLoggedIn)
+    if (ifStoredUserLoggedIn==='1') {
+      // after setting it function will run again.
+
+      setIsLoggedIn(true);
+    }
+    return () => {};
+  }, []);
 
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
     setIsLoggedIn(true);
+    localStorage.setItem("isLoggedIn", "1");
   };
 
   const logoutHandler = () => {
     setIsLoggedIn(false);
+
   };
 
   return (
