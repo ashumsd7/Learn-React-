c63501ea629e1e92040a2a7b2c8af27ace3de03a
aathym <aat8001@gmail.com>
2022-08-12 10:02:00 +0530

    init advance project for hooks

--- 3 Advanced/State Hooks/src/App.js
+++ 3 Advanced/State Hooks/src/App.js
@@ -1,36 +1,20 @@
-import React, { useState , useEffect} from "react";
+import React, { useState } from 'react';
 
-import Login from "./components/Login/Login";
-import Home from "./components/Home/Home";
-import MainHeader from "./components/MainHeader/MainHeader";
+import Login from './components/Login/Login';
+import Home from './components/Home/Home';
+import MainHeader from './components/MainHeader/MainHeader';
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
- 
 
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
     setIsLoggedIn(true);
-
-    localStorage.setItem("isLoggedIn", "1");
   };
-
-
-  // runs only once if no dependency passed
-
-  useEffect(()=>{
-console.log("WILL RUN ONLY ONCE ")
-    const isUserLoggedIn = localStorage.getItem("isLoggedIn");
-    if (isUserLoggedIn === "1") {
-      setIsLoggedIn(true);
-    }
-
-  },[])
 
   const logoutHandler = () => {
     setIsLoggedIn(false);
-    localStorage.setItem("isLoggedIn", "0");
   };
 
   return (
