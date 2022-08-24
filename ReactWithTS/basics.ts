418663e25c5ae5ad5b5b6c4abc2c31eaf46336b3
aathym <aat8001@gmail.com>
2022-08-24 12:07:39 +0530

    init react-typescript learn Primitives Typescript

---
+++ ReactWithTS/basics.ts
@@ -0,0 +1,55 @@
+// Primitives
+
+let age: number;
+
+
+age=12
+
+
+let myName: string;
+
+
+myName='ashu'
+
+
+
+
+let isActive: boolean
+
+isActive=true
+
+
+
+let hobbies :string[]
+
+
+hobbies =['ashu', 'as']
+
+let person :{
+    name:string,
+    age:number
+}
+
+ person ={
+    name:'Max',
+    age: 32
+}
+
+
+// Array of Objects
+
+
+let people : {
+    name:string,
+    age:number
+}[];
+
+people = [{
+    name:'Hello',
+    age:12
+    
+}]
+
+
+
+
