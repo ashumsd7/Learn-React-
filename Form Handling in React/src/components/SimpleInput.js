8b24c220826b2b81982fce8efc30e3ea95d02f09
aathym <aat8001@gmail.com>
2022-08-24 11:11:29 +0530

    WIP: Handling input form

--- Form Handling in React/src/components/SimpleInput.js
+++ Form Handling in React/src/components/SimpleInput.js
@@ -1,12 +1,32 @@
+import React, {useState, useRef} from 'react'
 const SimpleInput = (props) => {
+
+
+  const [enteredName, setEnteredName] = useState('');
+ const inputValue= useRef()
+
+
+ const  nameInputChangeHandler = event =>{
+  setEnteredName(event.target.value)
+ }
+  
+ const formSubmissionHandler = event =>{
+
+  console.log(">>>>>>>>>>>>", inputValue)
+  console.log(event)
+  event.preventDefault();
+  console.log(enteredName)
+  
+ 
+ }
   return (
-    <form>
+    <form  onSubmit={formSubmissionHandler}>
       <div className='form-control'>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input ref='inputValue' type='text' id='name' onChange={nameInputChangeHandler}/>
       </div>
       <div className="form-actions">
-        <button>Submit</button>
+        <button>Submit Form</button>
       </div>
     </form>
   );
