1c3dcbe06541ed485a3e0a588add9aaa9fcb978f
aathym <aat8001@gmail.com>
2022-09-27 19:28:25 +0530

    downgrade router

--- ReactRouter/learn-router/src/App.js
+++ ReactRouter/learn-router/src/App.js
@@ -1,11 +1,19 @@
-import './App.css';
+import "./App.css";
 
-import { Route} from 'react-router-dom'
+import { Route } from "react-router-dom";
+import Welcome from "./components/Welcome";
+import Products from "./components/Products";
 
 function App() {
   return (
     <div className="App">
-      
+      <Route path="/welcome">
+        <Welcome />
+      </Route>
+
+      <Route path="/products">
+        <Products />
+      </Route>
     </div>
   );
 }
