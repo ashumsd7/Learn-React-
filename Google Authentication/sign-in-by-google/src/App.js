fd27ba831eac4854c36010e334bd522c6f1ab4b5
aathym <aat8001@gmail.com>
2022-09-28 18:33:17 +0530

    add pages

--- Google Authentication/sign-in-by-google/src/App.js
+++ Google Authentication/sign-in-by-google/src/App.js
@@ -1,10 +1,25 @@
-
-import './App.css';
+import { Route } from "react-router-dom";
+import "./App.css";
+import NavBar from "./components/NavBar";
+import Account from "./pages/Account";
+import Home from "./pages/Home";
+import Signin from "./pages/Signin";
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
    
+        <Route path="/" >
+        <Home />
+        </Route>
+        <Route path="/signin" >
+        <Signin/>
+        </Route>
+        <Route path="/account">
+        <Account />
+        </Route>
+      
     </div>
   );
 }
---
+++ Google Authentication/sign-in-by-google/src/pages/Signin.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { GoogleButton } from "react-google-button";
+
+function Signin() {
+  return (
+    <>
+      <GoogleButton />
+    </>
+  );
+}
+
+export default Signin;
