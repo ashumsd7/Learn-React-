3c03c8b0085d9f8eee9e58a218adedbaa569c366
aathym <aat8001@gmail.com>
2022-09-29 01:16:50 +0530

    add porfolio

---
+++ Google Authentication/sign-in-by-google/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCoBlIb53SLryHuEbosJ5rGNfYwARQQy7A",
+  authDomain: "auth-eaa29.firebaseapp.com",
+  projectId: "auth-eaa29",
+  storageBucket: "auth-eaa29.appspot.com",
+  messagingSenderId: "180348531110",
+  appId: "1:180348531110:web:dd7057c7f1dcb837a7df67"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
